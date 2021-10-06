8f81d66a8e7dc3ebbe8dd7492077d9e1f48d5b7a
RenanOfugi <renan.ofugi@gmail.com>
2021-10-06 00:58:46 -0300

    Criado projeto inicial

---
+++ src/main/java/com/games/ecommerce/gamespayment/GamesPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.games.ecommerce.gamespayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/games/ecommerce/gamespayment/GamesPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.ecommerce.gamespayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
