c07f311d114fff127041aa121202d6174e17a2c0
RenanOfugi <renan.ofugi@gmail.com>
2021-10-12 00:22:20 -0300

    refactor: add dados de pagamnto no BD

--- src/main/java/com/games/ecommerce/gamespayment/service/GamesPaymentService.java
+++ src/main/java/com/games/ecommerce/gamespayment/service/GamesPaymentService.java
@@ -1,6 +1,7 @@
 package com.games.ecommerce.gamespayment.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.games.ecommerce.gamespayment.entity.CheckoutPaymentEntity;
 import com.games.ecommerce.gamespayment.repository.CheckoutGamesPaymentRepository;
@@ -23,19 +24,24 @@ public class GamesPaymentService {
     private void saveGamesCheckout(ConsumerRecord<String, String> record){
         log.info("chave = {}", record.key());
         log.info("cabeçalho = {}", record.headers());
+        log.info("Partição = {}", record.partition());
         log.info("Valores = {}", record.value());
 
         String data = record.value();
 
         ObjectMapper mapper = new ObjectMapper();
-        CheckoutPaymentEntity entity = null;
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        CheckoutPaymentEntity entity;
 
         try {
             entity = mapper.readValue(data, CheckoutPaymentEntity.class);
-	        System.out.println(entity.getCode());
+	        System.out.println("Code: " + entity.getCode() + " - Recebido os dados de checkout com sucesso!");
+            repository.save(entity);
         } catch (JsonProcessingException e) {
+            System.out.println("-------------------------------------");
             System.out.println("Não foi possível realizar a conversão");
+            e.printStackTrace();
+            System.out.println("-------------------------------------");
         }
-
     }
 }
