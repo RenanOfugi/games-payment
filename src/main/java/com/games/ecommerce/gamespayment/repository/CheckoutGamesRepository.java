9607ef0c7700855c8b8daf3b80ed6361b3480809
RenanOfugi <renan.ofugi@gmail.com>
2021-10-11 21:30:49 -0300

    Refactor: Modificações para o spring reconhecer o Lombok

--- src/main/java/com/games/ecommerce/gamespayment/repository/CheckoutGamesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.games.ecommerce.gamespayment.repository;
-
-import com.games.ecommerce.gamespayment.entity.CheckoutEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CheckoutGamesRepository extends JpaRepository<CheckoutEntity, Long> {
-}
--- src/main/java/com/games/ecommerce/gamespayment/service/GamesPaymentService.java
+++ src/main/java/com/games/ecommerce/gamespayment/service/GamesPaymentService.java
@@ -2,15 +2,22 @@ package com.games.ecommerce.gamespayment.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.games.ecommerce.gamespayment.entity.CheckoutEntity;
+import com.games.ecommerce.gamespayment.entity.CheckoutPaymentEntity;
+import com.games.ecommerce.gamespayment.repository.CheckoutGamesPaymentRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GamesPaymentService {
+
+    @Autowired
+    private final CheckoutGamesPaymentRepository repository;
 
     @KafkaListener(topics = "SalvaCheckout", groupId = "GamesMicroservices")
     private void saveGamesCheckout(ConsumerRecord<String, String> record){
@@ -21,14 +28,14 @@ public class GamesPaymentService {
         String data = record.value();
 
         ObjectMapper mapper = new ObjectMapper();
-        CheckoutEntity entity = null;
+        CheckoutPaymentEntity entity = null;
 
         try {
-            entity = mapper.readValue(data, CheckoutEntity.class);
+            entity = mapper.readValue(data, CheckoutPaymentEntity.class);
+	        System.out.println(entity.getCode());
         } catch (JsonProcessingException e) {
             System.out.println("Não foi possível realizar a conversão");
         }
-
 
     }
 }
